9fbc228a0a0c47b25674a40614bd103a836410df
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-07-11 14:02:44 +0530

    Update LastS3.py

--- LastS3.py
+++ LastS3.py
@@ -1,13 +1,19 @@
 import boto3
 
-def get_last_added_file(bucket_name):
+def get_last_added_file(bucket_name, folder_path):
     s3 = boto3.client('s3')
 
-    # List objects in the bucket
-    response = s3.list_objects_v2(Bucket=bucket_name)
+    # List objects in the bucket within the specified folder path
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_path)
+
+    # Filter objects to exclude subfolders
+    objects = [obj for obj in response['Contents'] if not obj['Key'].endswith('/')]
+
+    if not objects:
+        return None
 
     # Sort objects based on LastModified timestamp in descending order
-    sorted_objects = sorted(response['Contents'], key=lambda obj: obj['LastModified'], reverse=True)
+    sorted_objects = sorted(objects, key=lambda obj: obj['LastModified'], reverse=True)
 
     # Retrieve the key (file path) of the last added file
     last_added_key = sorted_objects[0]['Key']
@@ -16,8 +22,13 @@ def get_last_added_file(bucket_name):
 
 # Usage example
 bucket_name = 'your-bucket-name'
+folder_path = 'path/to/folder/'
 
-last_added_file = get_last_added_file(bucket_name)
+last_added_file = get_last_added_file(bucket_name, folder_path)
 
-# Print the key (file path) of the last added file
-print(last_added_file)
+if last_added_file:
+    # Print the key (file path) of the last added file
+    print(last_added_file)
+else:
+    print("No files found in the specified folder.")
+    
