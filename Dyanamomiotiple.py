3950f24d17050fc5dcb5f703e2f1b1076e642aa6
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-07-18 10:48:42 +0530

    Update Dyanamomiotiple.py

--- Dyanamomiotiple.py
+++ Dyanamomiotiple.py
@@ -13,6 +13,38 @@ dynamodb = session.client('dynamodb')
 # Define query parameters
 table_name = 'YourTableName'
 column_name = 'YourColumnName'
+desired_value = 'YourDesiredValue'
+
+# Construct the query
+response = dynamodb.query(
+    TableName=table_name,
+    KeyConditionExpression="#col = :value",
+    ExpressionAttributeNames={"#col": column_name},
+    ExpressionAttributeValues={":value": {"S": desired_value}}
+)
+
+# Retrieve the queried items
+items = response['Items']
+
+# Process the queried items
+for item in items:
+    print(item)
+    
+import boto3
+
+# Configure AWS credentials and region
+session = boto3.Session(
+    aws_access_key_id='YOUR_ACCESS_KEY',
+    aws_secret_access_key='YOUR_SECRET_KEY',
+    region_name='us-west-2'  # replace with your desired region
+)
+
+# Create DynamoDB client
+dynamodb = session.client('dynamodb')
+
+# Define query parameters
+table_name = 'YourTableName'
+column_name = 'YourColumnName'
 desired_values = ['Value1', 'Value2', 'Value3']  # Add your desired values here
 
 # Construct the keys for BatchGetItem
