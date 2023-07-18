88bcf1f5853d8069193c891397870b45798b2969
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-07-18 12:02:06 +0530

    Update All.py

--- All.py
+++ All.py
@@ -12,18 +12,29 @@ column_name = 'your-column-name'
 # List to store column values
 column_values = []
 
-# Scan the table to retrieve all items
+# Perform initial scan operation
 response = dynamodb.scan(
     TableName=table_name
 )
 
 # Process the response and extract column values
-for item in response['Items']:
-    if column_name in item:
-        value = item[column_name]
-        value_type = list(value.keys())[0]
-        value_data = value[value_type]
-        column_values.append(value_data)
+while 'Items' in response:
+    for item in response['Items']:
+        if column_name in item:
+            value = item[column_name]
+            value_type = list(value.keys())[0]
+            value_data = value[value_type]
+            column_values.append(value_data)
+
+    # Check if there are more pages to scan
+    if 'LastEvaluatedKey' in response:
+        last_evaluated_key = response['LastEvaluatedKey']
+        response = dynamodb.scan(
+            TableName=table_name,
+            ExclusiveStartKey=last_evaluated_key
+        )
+    else:
+        break
 
 # Print the column values
 print("Values in column:", column_values)
