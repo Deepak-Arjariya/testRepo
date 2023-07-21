7ea2816676904235d47e6eb665b70134433db526
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-07-21 14:52:47 +0530

    Update Secretalue.py

--- Secretalue.py
+++ Secretalue.py
@@ -52,4 +52,34 @@ if __name__ == "__main__":
     if secret:
         print("Secret Retrieved:")
         print(secret)
+
+
+
+
+
+import boto3
+
+# Initialize the AWS Secrets Manager client
+secrets_manager = boto3.client('secretsmanager')
+
+def get_secret_string(secret_name):
+    try:
+        response = secrets_manager.get_secret_value(SecretId=secret_name)
+        if 'SecretString' in response:
+            secret_string = response['SecretString']
+            return secret_string
+        else:
+            print(f"SecretString not found for secret: {secret_name}")
+            return None
+    except Exception as e:
+        print(f"Error retrieving secret: {e}")
+        return None
+
+if __name__ == "__main__":
+    secret_name = input("Enter the name of the secret to retrieve: ")
+    secret = get_secret_string(secret_name)
+    if secret:
+        print("Secret Retrieved:")
+        print(secret)
+        
         
