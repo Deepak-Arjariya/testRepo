acf30f903e6690104b02d26902c73ca7fd87299f
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-08-08 17:57:17 +0530

    Update Socket.py

--- Socket.py
+++ Socket.py
@@ -64,3 +64,37 @@ receive_thread.start()
 while True:
     message = input("Enter a message: ")
     client_socket.send(message.encode())
+
+
+
+
+
+
+
+import socket
+import threading
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            print("\nReceived:", message)
+        except:
+            break
+
+host = socket.gethostname()
+port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+
+receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+receive_thread.start()
+
+try:
+    while True:
+        message = input("You: ")
+        client_socket.send(message.encode())
+except KeyboardInterrupt:
+    print("Connection closed.")
+    client_socket.close()
