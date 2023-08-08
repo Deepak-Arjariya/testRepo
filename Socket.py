209ecb0d3ee7ad64e6217f5bafd930431e436d89
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-08-08 17:50:55 +0530

    Update Socket.py

--- Socket.py
+++ Socket.py
@@ -1,51 +1,38 @@
 import socket
+import threading
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Get the local machine's name
+# Bind the socket to a host and port
 host = socket.gethostname()
 port = 12345
-
-# Bind the socket to a public host and port
 server_socket.bind((host, port))
 
 # Listen for incoming connections
-server_socket.listen(1)
+server_socket.listen()
 
-print("Server listening on port", port)
+clients = []
 
-# Accept a connection from a client
-client_socket, client_address = server_socket.accept()
-print("Connected to", client_address)
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+            print("Received:", message)
+            # Broadcast the message to all clients
+            for client in clients:
+                client.send(message.encode())
+        except:
+            break
 
-# Send a message to the client
-message = "Hello, client!"
-client_socket.send(message.encode())
+    clients.remove(client_socket)
+    client_socket.close()
 
-# Close the connection
-client_socket.close()
-
-
-
-
-
-
-import socket
-
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Get the local machine's name
-host = socket.gethostname()
-port = 12345
-
-# Connect to the server
-client_socket.connect((host, port))
-
-# Receive data from the server
-data = client_socket.recv(1024).decode()
-print("Received:", data)
-
-# Close the connection
-client_socket.close()
+while True:
+    client_socket, client_address = server_socket.accept()
+    print("Connected to", client_address)
+    clients.append(client_socket)
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
