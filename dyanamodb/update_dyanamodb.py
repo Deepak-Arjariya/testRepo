fe554efae5590dbe826609cbb85814485db84e42
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-08-11 18:12:32 +0530

    Update update_dyanamodb.py

--- dyanamodb/update_dyanamodb.py
+++ dyanamodb/update_dyanamodb.py
@@ -142,3 +142,67 @@ for index, row in df.iterrows():
     
     print(f"Item with PartitionKey {partition_key_value} updated successfully!")
     
+
+
+
+
+
+
+
+
+
+import boto3
+
+# Initialize the DynamoDB client
+dynamodb = boto3.client('dynamodb')
+
+# Specify the table name
+table_name = 'YourTableName'
+
+# Iterate through the DataFrame
+for index, row in df.iterrows():
+    partition_key_value = str(row['PartitionKeyColumn'])  # Assuming a partition key column in the DataFrame
+    
+    # Retrieve the existing item from DynamoDB
+    existing_item = dynamodb.get_item(
+        TableName=table_name,
+        Key={
+            'PartitionKey': {'S': partition_key_value}
+        }
+    ).get('Item', {})
+    
+    # Construct the UpdateExpression and ExpressionAttributeValues for attributes to update
+    update_expression = "SET "
+    expression_attribute_values = {}
+    
+    for column_name in df.columns:
+        if column_name != 'PartitionKeyColumn':
+            # Clean the column name to remove special characters
+            cleaned_column_name = column_name.replace("-", "_")  # Replace hyphens with underscores
+            
+            # If the attribute exists in the existing item, update it
+            if cleaned_column_name in existing_item:
+                update_expression += f" #{cleaned_column_name} = :{cleaned_column_name},"
+                expression_attribute_values[f":{cleaned_column_name}"] = {'S': str(row[column_name])}
+            # If the attribute doesn't exist, add it
+            else:
+                update_expression += f" #{cleaned_column_name} = :{cleaned_column_name},"
+                expression_attribute_values[f":{cleaned_column_name}"] = {'S': str(row[column_name])}
+    
+    update_expression = update_expression.rstrip(',')
+    
+    # Update the item in DynamoDB
+    response = dynamodb.update_item(
+        TableName=table_name,
+        Key={
+            'PartitionKey': {'S': partition_key_value}
+        },
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=expression_attribute_values,
+        ExpressionAttributeNames={
+            f"#{cleaned_column_name}": column_name for column_name in df.columns if column_name != 'PartitionKeyColumn'
+        }
+    )
+    
+    print(f"Item with PartitionKey {partition_key_value} updated successfully!")
+    
