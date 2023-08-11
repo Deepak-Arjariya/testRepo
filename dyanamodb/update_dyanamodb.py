8bbd741a26c8905ca70a1708d7326a5425545d5e
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-08-11 12:57:02 +0530

    Update update_dyanamodb.py

--- dyanamodb/update_dyanamodb.py
+++ dyanamodb/update_dyanamodb.py
@@ -5,12 +5,16 @@ import openpyxl
 workbook = openpyxl.load_workbook('data.xlsx')
 sheet = workbook.active
 
+# Get header row for column mapping
+header_row = next(sheet.iter_rows(min_row=1, max_row=1, values_only=True))
+header_mapping = {value: index for index, value in enumerate(header_row)}
+
 # Initialize DynamoDB client
 dynamodb = boto3.client('dynamodb', region_name='your_region_name')
 
 # Iterate through the rows in the XLSX file
 for row in sheet.iter_rows(min_row=2, values_only=True):
-    partition_key = row[0]  # Assuming the partition key is in the first column
+    partition_key = row[header_mapping['PartitionKey']]  # Replace 'PartitionKey' with your actual header name
 
     # Retrieve existing item from DynamoDB
     response = dynamodb.get_item(
@@ -21,8 +25,8 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     existing_item = response.get('Item', {})
 
     # Merge data from Excel sheet with existing item, overwriting existing values
-    for col_index, update_value in enumerate(row[1:], start=1):
-        col_name = f'Column{col_index}'  # Adjust column naming as needed
+    for header, update_value in row[1:]:
+        col_name = header  # Assuming header is a valid attribute name
 
         if update_value:
             existing_item[col_name] = {'S': update_value}
@@ -34,4 +38,4 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     )
 
     print(f"Updated item with partition key: {partition_key}")
-  
+    
