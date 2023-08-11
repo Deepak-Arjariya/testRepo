6505f9635176cc3fd231b813b9cc813cd91020f6
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-08-11 17:54:12 +0530

    Update update_dyanamodb.py

--- dyanamodb/update_dyanamodb.py
+++ dyanamodb/update_dyanamodb.py
@@ -32,4 +32,52 @@ for index, row in df.iterrows():
     )
     
     print(f"Item with PartitionKey {partition_key_value} updated successfully!")
+
+
+
+
+
+
+
+import boto3
+
+# Initialize the DynamoDB client
+dynamodb = boto3.client('dynamodb')
+
+# Specify the table name
+table_name = 'YourTableName'
+
+# Iterate through the DataFrame
+for index, row in df.iterrows():
+    partition_key_value = str(row['PartitionKeyColumn'])  # Assuming a partition key column in the DataFrame
+    
+    # Delete the old item
+    delete_response = dynamodb.delete_item(
+        TableName=table_name,
+        Key={
+            'PartitionKey': {'S': partition_key_value}
+        }
+    )
+    
+    print(f"Old item with PartitionKey {partition_key_value} deleted successfully!")
+    
+    # Create a new item
+    new_item = {
+        'PartitionKey': {'S': partition_key_value}
+    }
+    
+    for column_name in df.columns:
+        if column_name != 'PartitionKeyColumn':
+            # Clean the column name to remove special characters
+            cleaned_column_name = column_name.replace("-", "_")  # Replace hyphens with underscores
+            
+            new_item[cleaned_column_name] = {'S': str(row[column_name])}
+    
+    # Insert the new item
+    put_response = dynamodb.put_item(
+        TableName=table_name,
+        Item=new_item
+    )
+    
+    print(f"New item with PartitionKey {partition_key_value} created successfully!")
     
