0141f91329fee8c0e980791fcca58dbdb21dacfa
Deepak-Arjariya <53655382+Deepak-Arjariya@users.noreply.github.com>
2023-09-02 16:59:42 +0530

    Update Add key to dyanamo.py

--- old/Add key to dyanamo.py
+++ old/Add key to dyanamo.py
@@ -1,19 +1,27 @@
-import boto3
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chat</title>
+  </head>
+  <body>
+    <ul id="messages"></ul>
+    <input id="m" autocomplete="off" /><button>Send</button>
+    <script src="/socket.io/socket.io.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+      const socket = io();
 
-def add_update_column(table_name, unique_key, column_name, column_value):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(table_name)
-    
-    response = table.update_item(
-        Key={ 'unique_key_name': unique_key },  # Replace 'unique_key_name' with the actual unique key attribute name
-        UpdateExpression=f"set {column_name} = :value",  # Replace 'column_name' with the actual column name
-        ExpressionAttributeValues={ ":value": column_value }
-    )
-    
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        print(f"Successfully added/updated column '{column_name}' for row with unique key '{unique_key}'")
-    else:
-        print("Failed to add/update column")
+      // Send chat messages
+      $('form').submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+      });
 
-# Example usage
-add_update_column('YourTableName', 'unique_key_value', 'column_name', 'column_value')
+      // Receive and display chat messages
+      socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+      });
+    </script>
+  </body>
+</html>
